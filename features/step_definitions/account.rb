842a595b72fb77119768a693dd54d3b5d78e693c
Arie <git@ariekanarie.nl>
2013-04-05 14:44:16 +0200

    remove features dir

--- features/step_definitions/account.rb
+++
@@ -1,4 +0,0 @@
-Given "I am logged in" do
-  @current_user = create(:user, :uid => '12345')
-  login_as(@current_user, :scope => :user)
-end
--- features/support/server_logs.rb
+++
@@ -1,5 +0,0 @@
-After('@logs') do
-  if @reservation.id
-    FileUtils.rmdir Rails.root.join('server_logs', "#{@reservation.id}")
-  end
-end
